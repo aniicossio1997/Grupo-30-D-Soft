71861e3a6726eb777d83330634d701cf5db19b23
luciano <luchonavarro877@gmail.com>
2018-06-05 20:49:31 -0300

    Actualizado

--- inicio.php
+++ inicio.php
@@ -1,12 +1,20 @@
 <?php
  include('header.php'); 
- $id = $verificar->id(); //obtengo la id del usuario que inicio la sesion 
+ //obtengo la id del usuario que inicio la sesion
+ $id = $verificar->id();  
+ //hago consulta (1) para obtener los vehiculos pertenecientes al usuario en secion
+ $consul = ("SELECT id FROM vehiculo WHERE usuario_id = $id"); 
+
+//hago una consulta (2) para obteber todos los viajes
  $consulta= ("SELECT * FROM viajes");
+ //ejecuto la consulta (2) y lo guardo en la variable resultado
  $resultado = mysqli_query($link, $consulta);
 ?>
- 	<h1 class="h1-form"> Publicaciones </h1> 
+
+  	<h1 class="h1-form"> Publicaciones </h1> 
  <?php
- while ($fila = mysqli_fetch_array($resultado)) {?>
+ //(while) voy obteniendo los datos de cada fila correspondiente a los viajes
+ while ($fila = mysqli_fetch_array($resultado)){ ?> 
  	<article class="mis_vehiculos"><p class="text_center">
 	  <article class="mis_vehiculos">	
 		<table class="tabla" >
@@ -15,32 +23,42 @@
 					<p class="p-perfil p-left">Origen: <?php echo ($fila['origen']); ?>
 					</p>
 				</td>
-		   	 	<td  class="td-p">
-		    	 	<p class="p-perfil p-left ">Destino: <?php echo ($fila['destino']); ?>
+		   	 	<td >
+		    	 	<p  class="p-perfil p-left ">Destino: <?php echo ($fila['destino']); ?>
 		    	 		
 		    	 	</p>
 		    	</td>
-		    	<td class=" td-a "> 
-		    		<a  class="a-link2 a-rig fondo-blue" href="">Postularse 
-		    		</a>
-		    	</td>		   
+	   
 		    </tr>
 		    <tr>		    
 		    	<td class="td-p"> 
-		    		<p class="p-perfil p-left ">Precio: <?php echo ($fila['costo']); ?>
-		    			
+		    		<p class="p-perfil p-left ">Precio: $<?php echo ($fila['costo']); ?>		
 		    		</p>
-		    	</td>
-
-		    	<td class="td-p"> 
-		    		<p class="p-perfil p-left "> </p>
-		    	</td>
-
-		    	<td class="td-a">
-		    		<a class="a-link2 a-rig fondo-blue" href="">Detalles 
+		    	</td>		    	
+		    </tr>		 
+			<tr>
+		   		 <td >
+		    		<a class="a-link2 a-rig fondo-blue"  href="">Detalles 
 		    		</a>
 		    	</td>
-		    </tr>
+		    	<td > 
+		    		<a  class="a-link2 a-rig fondo-blue" href="">Postularse 
+		    		</a>
+		    	</td>	
+					<?php 
+						 //ejecuto la consulta (1)
+					     $resul = mysqli_query($link, $consul);
+					     //(while) verifico sin el id del vehiculo de la publicacion actual coincide con un vehiculo del usuario en sesion.
+						while ( $user = mysqli_fetch_array($resul) ){
+						  //(if) si conciden agrego la opcion modificar a la publicacion				
+ 			              if ($fila['vehiculo_id'] == $user['id']) {
+					?>
+							<td style="width: 1%">
+						     <a class="a-link2 a-rig fondo-blue " href="">Modificar
+						     </a>
+						    </td>
+		      </tr>
+				    <?php }} ?>
 
 		</table>
 	</article>
