a63ade8effa59844a05854d0732ef59b467c7cd9
ana <anacossio7@gmail.com>
2018-07-07 16:36:00 -0300

    pago de tarjeta y estitica

--- alta_postulacion.php
+++ alta_postulacion.php
@@ -9,7 +9,7 @@ $verificar = new validar($link);//se crea  una clase
 $id= $verificar->id();
 
 //consulta para obtener la fecha y horario del viaje al que se quiere postular
-$consulta_f= "SELECT fecha, horario FROM viajes where id = $_GET[id_viaje]";
+$consulta_f= "SELECT fecha, horario FROM viajes where id = $_POST[id_viaje]";
 $resultado_f = mysqli_query($link,$consulta_f);
 $fila_f = mysqli_fetch_array($resultado_f);
 //si el viaje expiro
@@ -54,19 +54,19 @@ $consulta1 = "SELECT * FROM postulantes WHERE postulante_id=$id";
 $resultado1 = mysqli_query($link,$consulta1);
 $existe = false;
 	while ($fila = mysqli_fetch_array($resultado1)) {
-		if ($fila['postulante_id'] == $_GET['id'] && $fila['viaje_id'] == $_GET['id_viaje']) {
+		if ($fila['postulante_id'] == $_POST['id'] && $fila['viaje_id'] == $_POST['id_viaje']) {
 
 			$existe = true;
 		}
 	}
 	if ($existe == false){
-		$consulta ="INSERT INTO postulantes(id, viaje_id, postulante_id, estado,rechazado,visto) VALUES (null, $_GET[id_viaje], $_GET[id], 1, 0,0) ";
+		$consulta ="INSERT INTO postulantes(id, viaje_id, postulante_id, estado,rechazado,visto) VALUES (null, $_POST[id_viaje], $_POST[id], 1, 0,0) ";
 		$resultado = mysqli_query($link,$consulta);
 		$_SESSION['mensaje']=" Postulacion exitosa!!";
 		header("Location:index.php"); 
 		die();
     }else{
-    		$consulta1 = " UPDATE postulantes SET estado = 1 where viaje_id = $_GET[id_viaje]";
+    		$consulta1 = " UPDATE postulantes SET estado = 1 where viaje_id = $_POST[id_viaje]";
     		$resultado1 = mysqli_query($link,$consulta1);
             $_SESSION['mensaje']=" Postulacion exitosa";
     		header("Location:inicio.php");
