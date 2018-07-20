e112aa1adaffc5ffefddef1eff1cc5cda236419c
ana <anacossio7@gmail.com>
2018-07-20 00:06:39 -0300

    borrado erroneo

--- js/cartel.js
+++
@@ -1,9 +0,0 @@
-function ocultar_cartel() {
-var cartel=document.getElementById('cartel');
-cartel.classList.add('ocultar');
-}
-
-window.onload = function(){
-	document.getElementById('cerrar').onclick = ocultar_cartel;
-
-}
